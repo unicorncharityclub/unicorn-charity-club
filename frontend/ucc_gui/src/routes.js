49737bcf216ba48f1b40c3998a3f698f8a0beb7c
Viraj Talaty <virajtalaty@gmail.com>
2020-02-03 16:22:27 -0700

    Display details view

--- frontend/ucc_gui/src/routes.js
+++ frontend/ucc_gui/src/routes.js
@@ -7,7 +7,7 @@ import ArticleDetail from './containers/ArticleDetailView';
 const BaseRouter = () => (
     <div>
         <Route exact path='/' component={ArticleList} />
-        <Route exact path='/:articleID' component={ArticleDetail} />
+        <Route exact path='/articles/:articleID' component={ArticleDetail} />
     </div>
 );
 
