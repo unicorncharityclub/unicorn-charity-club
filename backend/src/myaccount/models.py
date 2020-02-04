d8d3376ad487628224fdeaeaaf5e40e5292847c5
Janice Abraham <janicejabraham@gmail.com>
2020-02-04 02:31:32 -0700

    Updating the myaccount model

--- backend/src/myaccount/models.py
+++ backend/src/myaccount/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Myaccount(models.Model):
-    title = models.CharField(max_length=120)
-    content = models.TextField()
+    Name = models.CharField(max_length=120)
+    Address = models.TextField()
+    Email = models.CharField(max_length=120, default='abc')
+    Mobile = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.title
+        return '{} {} {} {}'.format(self.Name, self.Address, self.Email, self.Mobile)
