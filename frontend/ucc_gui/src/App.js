c72b3e71ef1177cbadcd70a71303c59dd71c7ca7
Viraj Talaty <virajtalaty@gmail.com>
2020-02-05 17:34:45 -0700

    Sprint1_US3_Task20 Enabling registration from home page

--- frontend/ucc_gui/src/App.js
+++ frontend/ucc_gui/src/App.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import BaseRouter from './routes';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import BaseRouter from "./routes";
 import Navbar from "./components/Navbar/Navbar";
-
+import Register from "./components/Register/Register";
 import "./App.css";
 
 class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      //user: {}
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="content-wrapper">
-          <Router>
-            <Navbar />
-                <a href="/account">My Account Page</a>
+      <Router>
+        {this.state.user ? (
+          <div className="App">
+            <div className="content-wrapper">
+              <Navbar />
+              <a href="/account">My Account Page</a>
               <BaseRouter />
-          </Router>
-        </div>
-      </div>
+            </div>
+          </div>
+        ) : (
+          <div className="App">
+            <Register />
+          </div>
+        )}
+      </Router>
     );
   }
 }
--- frontend/ucc_gui/src/routes.js
+++ frontend/ucc_gui/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import ArticleList from './containers/ArticleListView';
-import Register from "./components/Register/Register";
+
 import ArticleDetail from './containers/ArticleDetailView';
 import Account from './components/Account/Account.js';
 
@@ -10,7 +10,7 @@ const BaseRouter = () => (
     <div>
         <Route exact path='/' component={ArticleList} />
         <Route exact path='/articles/:articleID' component={ArticleDetail} />
-        <Route exact path='/register' component={Register} />
+
         <Route exact path='/account/' component={Account} />
     </div>
 );
