d44bf690851ab96124cb185a8a0ae9e82424d64c
Janice Abraham <janicejabraham@gmail.com>
2020-02-05 22:03:29 -0700

    US#8 Task#27 updating model to upload pictures

--- backend/src/myaccount/models.py
+++ backend/src/myaccount/models.py
@@ -6,6 +6,7 @@ class Myaccount(models.Model):
     Address = models.TextField()
     Email = models.CharField(max_length=120, default='abc')
     Mobile = models.CharField(max_length=10)
+    ProfilePic = models.ImageField(upload_to='profilePictures', blank=True)
 
     def __str__(self):
-        return '{} {} {} {}'.format(self.Name, self.Address, self.Email, self.Mobile)
+        return '{} {} {} {}'.format(self.Name, self.Address, self.Email, self.Mobile, self.ProfilePic)
