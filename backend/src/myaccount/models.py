be90a1026834051fd8dc2e133a2a1b52b19c61b2
Janice Abraham <janicejabraham@gmail.com>
2020-02-08 00:32:48 -0700

    Adding validation for my accounts page

--- backend/src/myaccount/models.py
+++ backend/src/myaccount/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Myaccount(models.Model):
     Name = models.CharField(max_length=120)
     Address = models.TextField()
     Email = models.CharField(max_length=120, default='abc')
-    Mobile = models.CharField(max_length=10)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{11}$',
+                                 message="Phone number must be entered in the format: '+1xxxxxxxxxx'. Phone number should be 10 digits.")
+    #Mobile = models.CharField(max_length=10)
+    Mobile = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     ProfilePic = models.ImageField(upload_to='profilePictures', blank=True)
 
     def __str__(self):
