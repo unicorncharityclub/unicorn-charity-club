6e29f4d624a21b73d66d50a63fc8db31b9ec4efe
Jahnvi Rai <jrai4@asu.edu>
2020-02-07 19:56:57 -0700

    Updated api method to handle json US#3TASK#19

--- backend/src/accounts/api/views.py
+++ backend/src/accounts/api/views.py
@@ -1,17 +1,29 @@
+from django.db import DatabaseError
+from django.views.decorators.csrf import csrf_exempt
 from ..models import User
-from ..forms import UserRegisterForm
+import json
+from django.http import JsonResponse
 
-from django.shortcuts import render, redirect
 
-
+@csrf_exempt
 def register_user_view(request):
-    form = UserRegisterForm(request.POST)
+    response = {'status':"Invalid Request"}
     if request.method == 'POST':
-        if form.is_valid():
-            user = form.save()
-        else:
-            form = UserRegisterForm()
-        return redirect('/')
-    return render(request, 'register.html', {'form': form})
+        try:
+            json_data = json.loads(request.body)
+            first_name = json_data["first_name"]
+            last_name = json_data["last_name"]
+            email = json_data["email"]
+            password = json_data["password"]
+            dob = json_data["dob"]
+            user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=password,
+                                       dob=dob)
+            user.save()
+            response['status'] = "Success"
+        except ValueError:
+            response['status'] = "Invalid Request"
+        except DatabaseError:
+            response['status'] = "User Already Exists"
+    return JsonResponse(response)
 
 
