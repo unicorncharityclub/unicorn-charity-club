c5083fc8d887c228cc904cd3f59b49514fc75b36
Janice Abraham <janicejabraham@gmail.com>
2020-02-12 01:57:42 -0700

    adding server ip

--- backend/src/ucc/settings.py
+++ backend/src/ucc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$@^(pvzgs@t5chtsp(z^2o5l=t(g3sll-8v-r^(vrn)emoz=l#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.199.110.71']
 
 
 # Application definition
