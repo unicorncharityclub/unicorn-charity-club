964f1002d4490db0ed2c4ce0cedc631b47a82ac5
Jahnvi Rai <jrai4@asu.edu>
2020-02-16 22:05:23 -0700

    Fetch logged in user details and create the users account when user logs in US#52TASK#75

--- backend/src/myaccount/api/views.py
+++ backend/src/myaccount/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from ..models import Myaccount
 from .serializers import MyaccountSerializer
+from accounts.models import User
+from django.http import JsonResponse
 
 
 class MyaccountViewSet(viewsets.ModelViewSet):
@@ -9,3 +11,27 @@ class MyaccountViewSet(viewsets.ModelViewSet):
     """
     serializer_class = MyaccountSerializer
     queryset = Myaccount.objects.all()
+
+
+def get_account_details(request, user_id):
+    response = {'status': "Invalid Request"}
+    if request.method == 'GET':
+        try:
+            user = User.objects.get(pk=user_id)
+            if user:
+                name = user.first_name + user.last_name
+                email = user.email
+                address = user.myaccount.Address
+                mobile = user.myaccount.Mobile
+                response['status'] = '"Success'
+                response["name"] = name
+                response['email'] = email
+                response['address'] = address
+                response['mobile'] = mobile
+            else:
+                response['status'] = "Wrong user id"
+        except ValueError:
+            response['status'] = "Invalid Request"
+    return JsonResponse(response)
+
+
