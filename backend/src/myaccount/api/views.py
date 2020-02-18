f85b2eb61c9d3902b4d33d881994600c9d26bc78
Jahnvi Rai <jrai4@asu.edu>
2020-02-18 00:53:57 -0700

    Fix the frontend to get and update user account details US#52TASK#76

--- backend/src/myaccount/api/views.py
+++ backend/src/myaccount/api/views.py
@@ -1,37 +1,35 @@
 from rest_framework import viewsets
-from ..models import Myaccount
-from .serializers import MyaccountSerializer
+import json
 from accounts.models import User
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 class MyaccountViewSet(viewsets.ModelViewSet):
-    """
-    A viewset for viewing and editing user instances.
-    """
-    serializer_class = MyaccountSerializer
-    queryset = Myaccount.objects.all()
 
-
-def get_account_details(request, user_id):
-    response = {'status': "Invalid Request"}
-    if request.method == 'GET':
-        try:
-            user = User.objects.get(pk=user_id)
-            if user:
-                name = user.first_name + user.last_name
-                email = user.email
-                address = user.myaccount.Address
-                mobile = user.myaccount.Mobile
-                response['status'] = '"Success'
-                response["name"] = name
-                response['email'] = email
-                response['address'] = address
-                response['mobile'] = mobile
-            else:
-                response['status'] = "Wrong user id"
-        except ValueError:
-            response['status'] = "Invalid Request"
-    return JsonResponse(response)
-
-
+    @csrf_exempt
+    def get_account_details(request, user_id):
+        response = {'status': "Invalid Request"}
+        user = User.objects.get(pk=user_id)
+        json_data = json.loads(request.body)
+        if request.method == 'GET':
+            try:
+                if user:
+                    name = user.first_name + user.last_name
+                    email = user.email
+                    address = user.myaccount.Address
+                    mobile = user.myaccount.Mobile
+                    response['status'] = '"Success'
+                    response["name"] = name
+                    response['email'] = email
+                    response['address'] = address
+                    response['mobile'] = mobile
+                else:
+                    response['status'] = "Wrong user id"
+            except ValueError:
+                response['status'] = "Invalid Request"
+        elif request.method == 'PUT':
+            user.myaccount.Address = json_data['Address']
+            user.myaccount.Mobile = json_data['Mobile']
+            user.save()
+        return JsonResponse(response)
