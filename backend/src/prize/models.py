bffde9aac924ac313dec1edf9f9b9c303d812680
Janice Abraham <janicejabraham@gmail.com>
2020-02-22 00:16:19 -0700

    #T73 adding folder for images

--- backend/src/prize/models.py
+++ backend/src/prize/models.py
@@ -5,7 +5,7 @@ class Prize(models.Model):
     Category = models.CharField(max_length=120)
     Tags = models.CharField(max_length=120)
     ImageName = models.CharField(max_length=120)
-    Image = models.ImageField(upload_to='upload', blank=True)
+    Image = models.ImageField(upload_to='prizeImages')
 
     def __str__(self):
         return '{} {} {} {}'.format(self.Category,  self.Tags, self.ImageName, self.Image)
