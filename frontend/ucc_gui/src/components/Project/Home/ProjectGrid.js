672f0e4657a707295e44f3639256a6c293d1d4c6
aneeshdalvi <adalvi1@asu.edu>
2020-02-23 19:53:12 -0700

    Resolving Merge conflicts

--- frontend/ucc_gui/src/components/Project/Home/ProjectGrid.js
+++ frontend/ucc_gui/src/components/Project/Home/ProjectGrid.js
@@ -11,20 +11,12 @@ class ProjectGrid extends React.Component {
   render() {
     return (
       <div style={{ flexGrow: 1 }}>
-<<<<<<< HEAD
-        <Grid container spacing={2} direction="row" justify="flex-start" alignItems="flex-start"
-        >
-          {this.props.projectData.map(elem => (
-            <Grid item xs={12} sm={6} md={4} key={this.props.projectData.indexOf(elem)}>
-              <ImgMediaCard imageSrc={elem.image} id={elem.id} />                          
-=======
         <Grid container spacing={2} direction="row" justify="flex-start" alignItems="flex-start" style={{paddingLeft: "20px", paddingRight: "20px", paddingTop: "20px"}}>
           {this.props.projectData
               .filter(elem => elem.project_category.startsWith(this.props.category))
               .map(elem => (
             <Grid item xs={12} sm={6} md={6} key={this.props.projectData.indexOf(elem)} >
               <ImgMediaCard imageSrc={elem.project_banner} imageId={elem.project_id} onClick={this.projectSelectedHandler} />
->>>>>>> 6bf23f07f526419855c5804e95e7af7f055e4450
             </Grid>
           ))}
         </Grid>
