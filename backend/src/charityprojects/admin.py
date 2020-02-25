c3aa581582891f2857c68c7485a439522ff2bdee
Janice Abraham <janicejabraham@gmail.com>
2020-02-24 18:44:01 -0700

    Adding model file withing charityprojects

--- backend/src/charityprojects/admin.py
+++ backend/src/charityprojects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CharityProjects
+from .models import CharityProjects, Prize
+
 # Register your models here.
 
 admin.site.register(CharityProjects)
+admin.site.register(Prize)
--- backend/src/ucc/settings.py
+++ backend/src/ucc/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myaccount',
-    'prize',
     'corsheaders',
     'rest_framework',
     'accounts',
