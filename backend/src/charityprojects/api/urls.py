28981ed5eb6f3c8833183bea82e4e94f1a11f7d9
Jahnvi Rai <jrai4@asu.edu>
2020-03-03 03:46:55 -0700

    Method to update project status US108TASK124

--- backend/src/charityprojects/api/urls.py
+++ backend/src/charityprojects/api/urls.py
@@ -1,4 +1,4 @@
-from .views import charity_project_details, all_project_list, project_category, all_project_info_list, start_project, update_project_invitation_video_details, update_project_prize
+from .views import charity_project_details, all_project_list, project_category, all_project_info_list, start_project, update_project_invitation_video_details, update_project_prize, update_project_challenge_status_explore
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
@@ -9,6 +9,8 @@ urlpatterns = [
     path('category', project_category),
     path('start', start_project),
     path('invitationVideo', update_project_invitation_video_details),
-    path('projectPrize', update_project_prize)
+    path('projectPrize', update_project_prize),
+    path('update/Challenge1', update_project_challenge_status_explore)
+
 
 ]
