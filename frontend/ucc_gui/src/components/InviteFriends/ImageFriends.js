3f70b58d8ad2095b6073415575e8d80457b736e7
Viraj Talaty <virajtalaty@gmail.com>
2020-03-03 20:13:14 -0700

    Sprint3_US103_Task129 - added validations to the search fields input values

--- frontend/ucc_gui/src/components/InviteFriends/ImageFriends.js
+++ frontend/ucc_gui/src/components/InviteFriends/ImageFriends.js
@@ -4,10 +4,53 @@ import TextBlack from "../General/TextBlack";
 import TextBlackSubHeading from "../General/TextBlackSubHeading";
 import Search from "@material-ui/icons/Search";
 import Button from "@material-ui/core/Button";
+import cookie from "react-cookies";
+import AlertMessage from "../AlertMessage";
 
 
 class InviteFriends extends React.Component {
+    constructor(props) {
+            super(props);
+            this.state = {
+                friendName: '',
+                friendEmailid : '',
+                searchStatus : ''
+            }
+         }
 
+ onFriendNameChange(e) {
+    this.setState({ friendName: e.target.value });
+    this.setState({ friendEmailid: '' });
+    this.setState({ searchStatus : ''});
+  }
+
+ onFriendEmailidChange(e) {
+    this.setState({ friendEmailid: e.target.value });
+    this.setState({ friendName: '' });
+    this.setState({ searchStatus : ''});
+  }
+
+  onSearchFriends(e){
+        if(this.state.friendName)
+        {
+            this.setState({ searchStatus : "Search By Name"});
+        }
+        else if(this.state.friendEmailid)
+        {
+            let emailValid = this.state.friendEmailid.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+              if (emailValid === null) {
+                this.setState({ searchStatus : "Invalid Email Id"});
+              }
+              else
+              {
+                  this.setState({ searchStatus : "Search By Email Id"});
+              }
+        }
+        else
+        {
+            this.setState({ searchStatus : "Enter some value to search by"});
+        }
+  }
 
     render() {
         return (
@@ -15,54 +58,68 @@ class InviteFriends extends React.Component {
                 <TextBlueHeading message="BUILD YOUR TEAM"/>
                 <TextBlack message="Build your team by inviting family and friends to your project."/>
                 <br/>
-                <div style={{width:"100%", height:"250px", padding:"10px", borderRadius: "10px", borderStyle:"solid"}}>
-                    <div style={{height:"65%"}}>
-                        <div style={{width:"48%", float:"left", textAlign:"center"}}>
-                            <TextBlackSubHeading message="Find Friends By Their Name"/>
-                            <br/>
-                            <div className="form-item" align="center">
+                <div style={{width:"100%", height:"260px", padding:"10px", borderRadius: "10px", borderStyle:"solid"}}>
+                    <div>
+                        <div style={{width:"100%", height:"50px", float:"left", textAlign:"left"}}>
+                            <div style={{width:"5%",float:"left"}}>
+                                &nbsp;
+                            </div>
+                            <div style={{width:"40%",float:"left"}}>
+                                <TextBlackSubHeading message="Find Friends By Their Name : "/>
+                            </div>
+                            <div className="form-item" style={{float:"left",width:"50%"}}>
                               <input
                                 name="friend-name"
                                 type="text"
                                 id="friend-name"
                                 placeholder="Enter Name"
+                                style={{width:"100%"}}
+                                value={this.state.friendName}
+                                onChange={this.onFriendNameChange.bind(this)}
                               />
                             </div>
                         </div>
 
-                        <div style={{width:"4%", float:"left"}}>
-
-                            <TextBlueHeading message="&nbsp;&nbsp;"/>
+                        <div style={{width:"100%", float:"left", textAlign:"center"}}>
                             <TextBlueHeading message="OR"/>
-                            <TextBlueHeading message="&nbsp;&nbsp;"/>
                         </div>
-                        <div style={{width:"48%", float:"left", textAlign:"center"}}>
-                        <TextBlackSubHeading message="Find Friends By Their Email-Id"/>
-                        <br/>
-                        <div className="form-item" align="center">
-                          <input
+
+                        <div style={{width:"100%", float:"left", textAlign:"left"}}>
+                            <div style={{width:"5%",float:"left"}}>
+                                &nbsp;
+                            </div>
+                            <div style={{width:"40%",float:"left"}}>
+                                <TextBlackSubHeading message="Find Friends By Their Email-Id : "/>
+                            </div>
+                            <div className="form-item" style={{float:"left",width:"50%"}}>
+                              <input
                             name="friend-emailid"
                             type="text"
                             id="friend-emailid"
                             placeholder="Enter EmailId"
+                            style={{width:"100%"}}
+                            value={this.state.friendEmailid}
+                            onChange={this.onFriendEmailidChange.bind(this)}
                           />
+                            </div>
                         </div>
-                    </div>
+
                     </div>
 
                     <div style={{height:"25%", width:"100%",  textAlign:"center"}}>
+
                         <Button
                             size="small"
                             variant="outlined"
                             startIcon={<Search />}
                             component="span"
                             style={{textTransform:"None"}}
+                            onClick={this.onSearchFriends.bind(this)}
                           >
                             <TextBlueHeading message="Search"/>
-
                           </Button>
+                            <AlertMessage alertMessage={this.state.searchStatus} />
                     </div>
-
                 </div>
             </div>
         );
