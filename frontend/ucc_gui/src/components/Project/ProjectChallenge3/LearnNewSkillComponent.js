3e3f4ec0f3e2aa2b41ac963051e0227d5545e3e3
Bhawana Prasad <bprasad1@asu.edu>
2020-03-11 14:59:53 -0700

    US#111 Task#127 Create user interface for the challenge of learning new skill.

--- frontend/ucc_gui/src/components/Project/ProjectChallenge3/LearnNewSkillComponent.js
+++ frontend/ucc_gui/src/components/Project/ProjectChallenge3/LearnNewSkillComponent.js
@@ -1,46 +1,140 @@
 import React from 'react';
 import {Player} from "video-react";
-import TextBlueHeading from "../../General/TextBlueHeading";
-import Upload_photo from "../../../image/Default-profile-picture.png";
+import Upload_video from "../../../image/Settings_Camera.png";
+import "../../../containers/Projects/LearnNewSkill/LearnNewSkill.css";
+import Button from "react-bootstrap/Button";
+import axiosConfig from "../../../axiosConfig";
+import * as cookie from "react-cookies";
 
 
 class LearnNewSkillComponent extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            NewSkill: '',
+            Description: '',
+            Video: '',
+            finalVideo: '',
+            ProjectId: this.props.id,
+            Email: cookie.load('user_emailid')
+        }
+    };
+
+    defaultIfEmpty(value){
+        return value === "" ? "":value;
+    }
+
+    changeHandler = (event) =>{
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    videoHandler = (event) =>{
+       this.setState({
+           Video : URL.createObjectURL(event.target.files[0]),
+           finalVideo: event.target.files[0]
+        });
+    };
+
+
+    saveHandler(event,  requestType, id){
+        event.preventDefault();
+        let form_data = new FormData();
+        try {
+            form_data.append('NewSkill', this.state.NewSkill);
+            form_data.append('Description', this.state.Description);
+            if (this.state.finalVideo) {
+                form_data.append('Video', this.state.finalVideo, this.state.finalVideo.name);
+            }
+            form_data.append('ProjectId', this.state.ProjectId);
+            form_data.append('Email', this.state.UserEmailId);
+        } catch(err) {
+            console.log(err)
+        }
+
+        switch( requestType ) {
+            case 'post':
+            return axiosConfig.post('http://127.0.0.1:8000/learn_new_skill/', form_data,
+                    {
+                        headers: {
+                            'content-type': 'multipart/form-data'
+                        }
+                    })
+                    .then(res => {console.log(res)})
+                    .catch(error => console.log(error))
+            // case 'put':
+            //     return axiosConfig.put(`http://127.0.0.1:8000/learn_new_skill/${id}/`, form_data,
+            //         {
+            //             headers: {
+            //                 'content-type': 'multipart/form-data'
+            //             }
+            //         })
+            //         .then(res => console.log(res))
+            //         .catch(error => console.log(error))
+        }
+    };
+
+
     render() {
         return (
-            <div className="blessing-form">
-                  <div>
-                      <TextBlueHeading message="Challenge 3: Adventure"/>
-                      <br/>
-                      <label>LEARN A NEW SKILL</label>
-                      <br/>
-                      <label>Develop a new skill that will support the mission of the project.</label>
-                      <br/>
-                      <label>1. What new skill did you develop?</label>
-                      <br/>
-                      <input type="text"
-                            name="NewSkill"
-                            value=""
-                            onChange=""/>
-                      <br/>
-                      <label>2. Describe how you learned your new skill.</label>
-                      <br/>
-                      <input type="text"
-                             name="Description"
-                             value=""
-                             onChange="" />
-                      <br/>
-                      <label>3. Share a video or photo that celebrates your new skill.</label>
-                      <br/>
-                      <img className="profile-picture-list" src={Upload_photo} alt=""/>
-                      <label className="upload-photo" htmlFor="file">Upload/Create Video</label>
-                      <input id="file" style={{display: 'none'}}
-                                     type="file"
-                                     name="Photo"
-                                     accept=".png, .jpeg, .jpg"
-                                     onChange=""/>
-
-                  </div>
+            <div className="form-wrapper">
+                      <div className="project-header">
+                          <p>
+                              <img className="project-logo" src={Upload_video} alt="Avatar"/>
+                          </p>
+                          <p>
+                              <label>Charity Project</label>
+                              <label>Date Joined</label>
+                              <label>Status</label>
+                          </p>
+                      </div>
+                      <div className="challenge-name">
+                          <label>CHALLENGE 3: Adventure</label>
+                      </div>
+                      <div className="project-form">
+                          <label>LEARN A NEW SKILL</label>
+                          <label>Develop a new skill that will support the mission of the project.</label>
+                          <div className="project-form-inner">
+                              <label>1. What new skill did you develop?</label>
+                              <input type="text"
+                                    name="NewSkill"
+                                    value={this.defaultIfEmpty(this.state.NewSkill)}
+                                    onChange={this.changeHandler.bind(this)}/>
+                              <label>2. Describe how you learned your new skill.</label>
+                              <textarea name="Description"
+                                     value={this.defaultIfEmpty(this.state.Description)}
+                                     onChange={this.changeHandler.bind(this)} />
+                              <label>3. Share a video or photo that celebrates your new skill.</label>
+                          </div>
+                      </div>
+                    <div className="project-video-preview">
+                        {
+                              this.state.Video ?
+                                  <div className="video-upload-preview">
+                                      <Player playsInline
+                                          src={this.state.Video}
+                                      />
+                                  </div>:''
+                        }
+                    </div>
+                      <div className="project-video">
+                          <img className="project-video-upload" src={Upload_video} alt=""/>
+                          <input id="file" style={{display: 'none'}}
+                                         type="file"
+                                         name="Video"
+                                         accept="video/*"
+                                         onChange={this.videoHandler.bind(this)}/>
+                          <label className="upload-video" htmlFor="file">Upload/Create Video</label>
+                      </div>
+                <div className="navigate-save">
+                    {/*<label htmlFor="save">Save</label>*/}
+                    <Button className="save-button" id="save" variant="contained" type="submit"
+                            onClick={this.saveHandler.bind(this)}>SAVE</Button>
+                    <Button className="done-button" id="done" variant="contained" type="submit">DONE</Button>
+                </div>
             </div>
         );
     }
