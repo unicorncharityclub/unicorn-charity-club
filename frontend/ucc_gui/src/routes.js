de89ecb5f3a67fb8701051b5dbbf85ccff21b2ac
Bhawana Prasad <bprasad1@asu.edu>
2020-03-22 18:00:27 -0700

    Adding and updating reusable components.

--- frontend/ucc_gui/src/routes.js
+++ frontend/ucc_gui/src/routes.js
@@ -48,7 +48,6 @@ class BaseRouter extends React.Component {
               <Route exact path='/Projects/:id/StartProjectStepTwo' component={StartProjectStepTwo} />
               <Route exact path='/Projects/:id/InviteFriends' component={StartProjectStepThree} />
               <Route exact path='/Projects/:id/ActiveProjectChallenge1' component={ActiveProjectChallenge1} />
-              <Route exact path='/Projects/:id/LearnNewSkill' component={LearnNewSkill} />
               <Route exact path='/AddChild' component={AddChild}/>
               <Route exact path="/Mobile_toolbar" component={Mobile_toolbar}/>
             </div>
