a7145a12ac5caf381a2be82139efa1dcaff61fb7
aneeshdalvi <adalvi1@asu.edu>
2020-04-15 14:25:28 -0700

    Sprint6_US#231_Task#257 Created links to the footer element and allowed to footer to display for all pages of application

--- frontend/ucc_gui/src/containers/Template/Footer/Footer.js
+++ frontend/ucc_gui/src/containers/Template/Footer/Footer.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Container, Table, TableBody, TableHead } from "@material-ui/core";
 import "./Footer.css";
 
 class Footer extends React.Component {
   render() {
-    return (               
-        <div className="footer_container">
-                <p>This is my footer.. </p>
+    return (   
+      <div className="main_container">      
+          <div className="footer_container"> 
+            <div>
+                <a href="">Column 1</a> 
+                <a href="">Lorem ipum dolor</a>
+                <a href="">Telus ante nesr</a> 
+                <a href="">Dignestim elit imperdiet</a> 
+            </div>
+            <div>
+                <a href="">Column 2</a> 
+                <a href="">Lorem ipum dolor</a>
+                <a href="">Telus ante nesr</a> 
+                <a href="">Dignestim elit imperdiet</a> 
+            </div>
+            <div>
+                <a href="">Column 3</a> 
+                <a href="">Lorem ipum dolor</a>
+                <a href="">Telus ante nesr</a> 
+                <a href="">Dignestim elit imperdiet</a> 
+            </div>                        
+          </div>
+          
+          
+          <div className="allrights">
+            <p>
+              ALL RIGHTS RESERVED. 
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text.
+            </p>
+          </div>
         </div>                 
     );
   }
