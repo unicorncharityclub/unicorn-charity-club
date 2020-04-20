5807491d9c7be745bf085917f6b70160091e2262
Viraj Talaty <virajtalaty@gmail.com>
2020-04-19 17:17:56 -0700

    Sprint6_US233_Task238 - Serializer works for put also. Adding comments.

--- backend/src/profile/api/serializers.py
+++ backend/src/profile/api/serializers.py
@@ -14,6 +14,7 @@ def get_child_profile(user):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    # automatically relates the data to the 'name' field of the project interest table.
     project_interest = serializers.SlugRelatedField(many=True, read_only=False, slug_field='name', queryset=ProjectInterest.objects.all())
 
     class Meta:
