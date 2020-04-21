c656b72fbc5d449483fd409c5e5a7b7967878f53
Janice Abraham <janicejabraham@gmail.com>
2020-04-21 04:11:35 -0700

    Modifying dive donations and challenge 2 page

--- frontend/ucc_gui/src/components/Project/ActiveProject/Step_3/GiveADonation/GiveDonationDetails.js
+++ frontend/ucc_gui/src/components/Project/ActiveProject/Step_3/GiveADonation/GiveDonationDetails.js
@@ -111,62 +111,6 @@ class GiveDonationDetails extends React.Component {
                     </div>
                 </div>
             </div>
-
-// --------------------------------------------------------------------------------------------
-
-        //     <div style={{ margin: "10px" }}>
-        // <div style={{ marginBottom: "150px" }}>
-        //         <form onSubmit={this.handleFormSubmit}>
-        //             <div className="header_step_banner_common">
-        //             <div className="stepper_common">
-        //             <ProgressStepper currentStep="2" />
-        //             </div>
-        //             <div className="banner_common">
-        //             <ProjectBanner image={this.state.projectBanner}  />
-        //             </div>
-        //             </div>
-        //             <div className="content_project_info_vertical">
-        //             <ProjectInfo vertical={true} id={this.state.projectId} />
-        //             </div>
-        //         {/*<div className="project-form">*/}
-        //         <div className="content_section">
-        //         <div className="content_project_info">
-        //         <ProjectInfo id={this.state.projectId} />
-        //         </div>
-        //         <TextBlueHeading message="Challenge 3: Adventure"/>
-        //               <br/>
-        //               <br/>
-        //                 <TextBlackSubHeading message = "GIVE A DONATION"/>
-        //                 <br/>
-        //                 <TextBlackSubHeading message = "Give a donation to a local organization that supports the mission of the project."/>
-        //                 <div className="project-form-inner">
-        //                     <Address
-        //                         changeHandler = {this.props.changeHandler}/>
-
-        //                     <TextBlackSubHeading message = "3. Describe what you donated."/><br/>
-        //                       <TextArea name="description" rows={3} cols={80}
-        //                              value={this.props.defaultIfEmpty(this.props.description)}
-        //                              handleChange={this.props.changeHandler.bind(this)} /><br/>
-
-        //                              <TextBlackSubHeading message="3. Share a video or photo that celebrates your volunteer experience."/>
-        //                   </div>
-        //             <br/>
-        //             <Video src={this.props.video}
-        //                 id="file" style={{display: 'none'}}
-        //                    type="file"
-        //                    name="video"
-        //                    accept="video/*"
-        //                    onChange={this.props.videoHandler.bind(this)}/>
-        //                    <div> <br/>
-        //             <TwoButtonLayout button1Text="SAVE" button2Text="DONE"
-        //                    button1Click={(event) => this.props.onSubmit(event, 'Save')}
-        //                              button2Click={(event) => this.props.onSubmit(event, 'Done')}/>
-        //                    </div>
-
-        //           </div>
-        //         </form>
-        //     </div>
-        //     </div>
         );
     }
 }
