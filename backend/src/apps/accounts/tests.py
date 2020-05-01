00bd4547a7f8f8e849268ad3ef7ae36b89ce8667
Viraj Talaty <virajtalaty@gmail.com>
2020-04-30 23:13:23 -0700

    Test Cases for account, profile and prize

--- backend/src/apps/accounts/tests.py
+++ backend/src/apps/accounts/tests.py
@@ -1,42 +1,74 @@
-from django.test import RequestFactory, TestCase
-from rest_framework.test import force_authenticate
-from rest_framework.test import APIClient
-
-from apps.accounts.api.views import UserRegistrationView
-from apps.accounts.models import User
+from django.contrib.auth.hashers import make_password
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from apps.accounts.api.views import UserRegistrationView, AddChildView
+from apps.accounts.models import User
 
 
 # Create your tests here.
+from apps.profile.models import ChildProfile, Profile
+
+
 class AccountsTest(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(email='testuser@gmail.com', password='123456')
+        self.factory = APIRequestFactory(enforce_csrf_checks=False)
+        self.user = User.objects.create(email='testuser@gmail.com', password=make_password('123456'),
+                                        first_name='lorum', last_name='ipsum', gender='Male', dob='2008-03-26')
+
+    def helper_authenticate_user(self, request):
+        request.user = self.user
+        force_authenticate(request, user=self.user)
 
     def test_register_new_user(self):
+        """
+        The test method is used to test the registration of a new user to the application.
+        :return:
+        """
         request = self.factory.post('register/',
                                     {'email': 'testuser2@gmail.com',
                                      'password': '123456',
                                      'first_name': 'lorem',
                                      'last_name': 'ipsum',
-                                     'dob': '2018-02-02',
-                                     'gender': 'Male',
-                                     'mobile': '+18523697410'}
+                                     'dob': '2018-02-02'}
                                     )
         request.session = None
         response = UserRegistrationView.as_view()(request, *[], **{})
         self.assertEqual(response.data['status'], 'Success')
 
     def test_register_existing_user(self):
+        """
+        The test method is used to test the registration of an existing user to the application.
+        :return:
+        """
         request = self.factory.post('register/',
                                     {'email': 'testuser@gmail.com',
                                      'password': '123456',
                                      'first_name': 'lorem',
                                      'last_name': 'ipsum',
-                                     'dob': '2018-02-02',
-                                     'gender': 'Male',
-                                     'mobile': '+18523697410'}
+                                     'dob': '2018-02-02'}
                                     )
         request.session = None
         response = UserRegistrationView.as_view()(request, *[], **{})
         self.assertEqual(response.data['status'], 'User Already Exist')
+
+    def test_child(self):
+        """
+        This method is used to test the addition of a new child account for a parent.
+        :return:
+        """
+        request = self.factory.post('addchild/', {'first_name': 'child_fname',
+                                                  'last_name': 'lorum',
+                                                  'dob': '2016-03-24',
+                                                  'address': 'address_child',
+                                                  'about_me': 'child about details',
+                                                  'dream': 'child dreams',
+                                                  'school': 'ASU school',
+                                                  'school_grade': 'Grade 1'})
+        self.helper_authenticate_user(request)
+        response = AddChildView.as_view()(request, *[], **{})
+        self.assertEqual(response.status_code, 200)
+
+        # Get child details
+        child_object = ChildProfile.objects.filter(parent=self.user)[0]
+        self.assertEqual(child_object.school, 'ASU school')
--- backend/src/apps/profile/tests.py
+++ backend/src/apps/profile/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth.hashers import make_password
 from rest_framework.test import APIRequestFactory, force_authenticate
 
 from apps.accounts.models import User
-from django.test import TestCase, Client
+from django.test import TestCase
 
 # Create your tests here.
 from apps.profile.api.views import ProfileDetailView
@@ -12,14 +12,47 @@ from apps.profile.models import Profile
 class ProfileTest(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory(enforce_csrf_checks=False)
-        self.address = '123 E Unv Drive'
         self.user = User.objects.create(email='testuser@gmail.com', password=make_password('123456'),
                                         first_name='lorum', last_name='ipsum', gender='Male', dob='2008-03-26')
+        self.address = '186 N ave'
+        self.dream = 'inception dream'
+
+        profile_object = Profile.objects.get(user=self.user)
+        profile_object.address = self.address
+        profile_object.dream = self.dream
+        profile_object.save()
+
+    def helper_authenticate_user(self, request):
+        request.user = self.user
+        force_authenticate(request, user=self.user)
 
     def test_update_profile(self):
-        request = self.factory.put('view_profile/', {'address': self.address,
-                                                     'dob': '2018-02-02'})
-        force_authenticate(request, user=self.user)
+        """
+        Method to test the update profile method
+        :return:
+        """
+        address = '123 E Unv Drive'
+        dob = '2018-02-02'
+        request = self.factory.put('view_profile/', {'address': address, 'dob': dob, 'first_name': 'lorum',
+                                                     'last_name': 'ipsum', 'email': 'testuser@gmail.com'})
+        self.helper_authenticate_user(request)
         ProfileDetailView.as_view()(request, *[], **{})
-        profile_obj = (Profile.objects.get(user = self.user))
-        self.assertEqual(profile_obj.address, self.address)
+        profile_obj = (Profile.objects.get(user=self.user))
+
+        # Validating that the data has updated
+        self.assertEqual(profile_obj.address, address)
+        self.assertEqual(str(self.user.dob), dob)
+
+    def test_get_profile(self):
+        """
+        Method to test the get profile method which returns the complete profile of the user
+        :return:
+        """
+        request = self.factory.get('view_profile/')
+        self.helper_authenticate_user(request)
+        response = ProfileDetailView.as_view()(request, *[], **{})
+        data = response.data
+        # Validating that the response data
+        self.assertEqual(data['address'], self.address)
+        self.assertEqual(data['dream'], self.dream)
+
